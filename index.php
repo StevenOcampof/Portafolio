ea29145fd2d406a635cdaa2647302f6c49eaf131
StevenOcampof <jeison_ocampo@soy.sena.edu.co>
2025-07-26 14:29:04 -0500

    ajustes de las cards desde el css para el diseño de la interfaz de proyectos

--- index.php
+++ index.php
@@ -13,7 +13,9 @@
     <link href="./librerias/custom.css" rel="stylesheet" type="text/css"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bitcount+Grid+Single:wght@100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bitcount+Grid+Single:wght@100..900&display=swap"
+          rel="stylesheet">
+    <!--    <base href="imagenes/">-->
 </head>
 <body id="top">
 <nav class="navbar navbar-expand-lg p-2 shadow-lg sticky-top">
@@ -25,9 +27,13 @@
         </a>
 
         <!-- Botón hamburguesa -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu"
+        <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarMenu"
                 aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <!--            <span class="navbar-toggler-icon"></span>-->
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
         </button>
 
         <!-- Contenido colapsable -->
@@ -74,22 +80,125 @@
 
     <section class="my-2 mx-5" id="section-proyects">
         <div class="row justify-content-center align-items-center">
-            <div class="card col-sm-3 mx-5" style="width: 18rem;">
-                <img src="imagenes/Baga1.png" class="card-img-top" alt="index proyecto Bagatela Web">
+            <div class="card col-sm-3 mx-5 my-2" style="width: 25rem;">
+                <h4 class="card-title mt-3">Proyecto VR/Web</h4>
+                <div id="carouselBagaWeb" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselBagaWeb" data-bs-slide-to="0" class="active"
+                                aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselBagaWeb" data-bs-slide-to="1"
+                                aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselBagaWeb" data-bs-slide-to="2"
+                                aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="imagenes/Baga1.png" class="card-img-top p-2 img-card" ondragstart="return false;"
+                                 alt="index proyecto Bagatela Web">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imagenes/Baga2.png" class="d-block w-100 p-2 img-card" ondragstart="return false;"
+                                 alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imagenes/Baga3.png" class="d-block w-100 p-2 img-card" ondragstart="return false;"
+                                 alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev bg-black" type="button" data-bs-target="#carouselBagaWeb"
+                            data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next bg-black" type="button" data-bs-target="#carouselBagaWeb"
+                            data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
                 <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Participé en el desarrollo de un recorrido virtual del museo La Bagatela,
+                        creado en equipo con Unity. El proyecto permite explorar el museo de forma interactiva e
+                        inmersiva. Se programó en C# e incluimos elementos 3D y adaptación para la web.</p>
                 </div>
             </div>
-            <div class="card col-sm-3 mx-5" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+            <div class="card col-sm-3 mx-5 my-2" style="width: 25rem;">
+                <h4 class="card-title mt-3">Proyecto Productivas</h4>
+                <div id="carouselProductivas" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselProductivas" data-bs-slide-to="0" class="active"
+                                aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselProductivas" data-bs-slide-to="1"
+                                aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselProductivas" data-bs-slide-to="2"
+                                aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="imagenes/produ1.png" class="card-img-top p-2 img-card" ondragstart="return false;"
+                                 alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imagenes/produ2.png" class="d-block w-100 p-2 img-card"
+                                 ondragstart="return false;" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imagenes/produ3.png" class="d-block w-100 p-2 img-card"
+                                 ondragstart="return false;" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev bg-black" type="button" data-bs-target="#carouselProductivas"
+                            data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next bg-black" type="button" data-bs-target="#carouselProductivas"
+                            data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
                 <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Contribuí en la mejora de un aplicativo web para la gestión de etapas
+                        productivas, Durante el desarrollo, aporté nuevas funcionalidades y mejoras en la experiencia de
+                        usuario bajo el modelo MVC con PHP, JavaScript, HTML, CSS y Bootstrap.
+                    </p>
                 </div>
             </div>
-            <div class="card col-sm-3 mx-5" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+            <div class="card col-sm-3 mx-5 my-2" style="width: 25rem;">
+                <h4 class="card-title mt-3">Proyecto LabAires</h4>
+                <div id="carouselLaboratorio" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselLaboratorio" data-bs-slide-to="0" class="active"
+                                aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselLaboratorio" data-bs-slide-to="1"
+                                aria-label="Slide 2"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="imagenes/Lab1.png" class="card-img-top p-2 img-card" ondragstart="return false;"
+                                 alt="index proyecto Bagatela Web">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imagenes/Lab2.png" class="d-block w-100 p-2 img-card" ondragstart="return false;"
+                                 alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev bg-black" type="button" data-bs-target="#carouselLaboratorio"
+                            data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next bg-black" type="button" data-bs-target="#carouselLaboratorio"
+                            data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
                 <div class="card-body">
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p class="card-text">Participé en el desarrollo de un aplicativo enfocado en el control de los
+                        servicios prestados por la organización, con el objetivo de mejorar la gestión y el seguimiento
+                        de sus procesos operativos.</p>
                 </div>
             </div>
         </div>
@@ -125,12 +234,12 @@
 <footer class="text-center text-lg-start">
     <div class="text-center p-2">
         <p>
-           <small>
-               © 2025 Jeison Ocampo. Todos los derechos reservados.
-           </small>
+            <small>
+                © 2025 Jeison Ocampo. Todos los derechos reservados.
+            </small>
         </p>
     </div>
-<script src="./librerias/js/bootstrap5.min.js"></script>
-<?php include_once __DIR__ . '/app/librerias-js.php'; ?>
+    <script src="./librerias/js/bootstrap5.min.js"></script>
+    <?php include_once __DIR__ . '/app/librerias-js.php'; ?>
 </body>
 </html>
